14c2221ab21b69e726f1de02eb0b2bb2fcee8263
Richard Hofmänner <hello@terillos.dev>
2023-02-17 10:15:09 +0100

    Code formatted

--- Tag01/Code/2_data_types.py
+++ Tag01/Code/2_data_types.py
@@ -1,27 +1,34 @@
 # Integer
 my_int = 42
-print(f"my_int is of type {type(my_int)} and value is {my_int}") # Output: my_int is of type <class 'int'> and value is 42
+# Output: my_int is of type <class 'int'> and value is 42
+print(f"my_int is of type {type(my_int)} and value is {my_int}")
 
 # float
 my_float = 23.5
-print(f"my_float is of type {type(my_float)} and value is {my_float}") # Output: my_float is of type <class 'float'> and value is 23.5
+# Output: my_float is of type <class 'float'> and value is 23.5
+print(f"my_float is of type {type(my_float)} and value is {my_float}")
 
 # boolean
-my_bool = True # or False
-print(f"my_float is of type {type(my_bool)} and value is {my_bool}") # Output: my_float is of type <class 'bool'> and value is True
+my_bool = True  # or False
+# Output: my_float is of type <class 'bool'> and value is True
+print(f"my_float is of type {type(my_bool)} and value is {my_bool}")
 
 # strings
 my_str = "hello world"
-print(f"my_str is of type {type(my_str)} and value is '{my_str}'") # Output: my_str is of type <class 'str'> and value is 'hello world'
+# Output: my_str is of type <class 'str'> and value is 'hello world'
+print(f"my_str is of type {type(my_str)} and value is '{my_str}'")
 
 # list
 my_list = [my_int, my_float, my_bool, my_str, None]
-print(f"my_list is of type {type(my_list)} and value is {my_list}") # Output: my_list is of type <class 'list'> and value is [42, 23.5, True, 'hello world', None]
+# Output: my_list is of type <class 'list'> and value is [42, 23.5, True, 'hello world', None]
+print(f"my_list is of type {type(my_list)} and value is {my_list}")
 
 # dictionary
 my_dict = {'my_int': my_int, 'my_float': my_float, 'my_str': my_str}
-print(f"my_dict is of type {type(my_dict)} and value is {my_dict}") # Output: my_dict is of type <class 'dict'> and value is {'my_int': 42, 'my_float': 23.5, 'my_str': 'hello world'}
+# Output: my_dict is of type <class 'dict'> and value is {'my_int': 42, 'my_float': 23.5, 'my_str': 'hello world'}
+print(f"my_dict is of type {type(my_dict)} and value is {my_dict}")
 
 # tuple
 my_tuple = (my_int, my_float, my_bool, my_str, None)
-print(f"my_tuple is of type {type(my_tuple)} and value is {my_tuple}") # Output: my_tuple is of type <class 'tuple'> and value is (42, 23.5, True, 'hello world', None)
+# Output: my_tuple is of type <class 'tuple'> and value is (42, 23.5, True, 'hello world', None)
+print(f"my_tuple is of type {type(my_tuple)} and value is {my_tuple}")
--- Tag01/Code/3_list_usage.py
+++ Tag01/Code/3_list_usage.py
@@ -2,22 +2,24 @@ list = []
 
 list.append("first entry")
 list.append("second entry")
-list.append(range(3)) # append adds one entry, here a Range
-list.extend(range(3)) # append adds multiple entries, here a Range
-list.extend('abc') # append adds multiple entries, here 'a', 'b' and 'c'
-list.insert(2, "third entry") # inserts an entry at a certain position
+list.append(range(3))  # append adds one entry, here a Range
+list.extend(range(3))  # append adds multiple entries, here a Range
+list.extend('abc')  # append adds multiple entries, here 'a', 'b' and 'c'
+list.insert(2, "third entry")  # inserts an entry at a certain position
 
-print(f"list = {list}") # Output: list = ['first entry', 'second entry', 'third entry', range(0, 3), 0, 1, 2, 'a', 'b', 'c']
+# Output: list = ['first entry', 'second entry', 'third entry', range(0, 3), 0, 1, 2, 'a', 'b', 'c']
+print(f"list = {list}")
 
-print(f"index of element 'third entry': {list.index('third entry')}") # Output: index of element 'third entry': 2
+# Output: index of element 'third entry': 2
+print(f"index of element 'third entry': {list.index('third entry')}")
 
-del list[3] # deletes the 4th entry (-> zero based), here the Range object
-del list[3:5] #deletes the 4th and the 5th entry, here 0 and 1
-print(f"list = {list}") # Output: list = ['first entry', 'second entry', 'third entry', 0, 1, 2]
-print(f"len(list) = {len(list)}") # Output: len(list) = 7
+del list[3]  # deletes the 4th entry (-> zero based), here the Range object
+del list[3:5]  # deletes the 4th and the 5th entry, here 0 and 1
+# Output: list = ['first entry', 'second entry', 'third entry', 0, 1, 2]
+print(f"list = {list}")
+print(f"len(list) = {len(list)}")  # Output: len(list) = 7
 
 
 list.clear()
 
-print(f"len(list) = {len(list)}") # Output: len(list) = 0
-
+print(f"len(list) = {len(list)}")  # Output: len(list) = 0
