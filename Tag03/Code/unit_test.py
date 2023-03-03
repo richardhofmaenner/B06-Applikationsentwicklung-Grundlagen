265ab507ac5f136a672b4897013c7c6cd04d95d6
Richard Hofmänner <hello@terillos.dev>
2023-03-03 08:56:25 +0100

    Added files of day 3 and the solution to excercise "the gang"

---
+++ Tag03/Code/unit_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+import unittest
+
+def calculate_age_average(people_dict):
+    """Function to calculate the average age. people_dict is expected to be a dictionary with sub-dictionaries which contains 'Age'."""
+    pass
+
+class TestCalculateAverage(unittest.TestCase):
+    def test_when_empty_then_zero(self):
+        self.assertEqual(calculate_age_average({}), 0.0)
+
+    def test_when_none_then_zero(self):
+        self.assertEqual(calculate_age_average(None), 0.0)
+
+    def test_when_all_integer_1_then_one(self):
+        self.assertEqual(calculate_age_average({'a':{'Age':1}, 'b':{'Age':1}, 'b':{'Age':1}}), 1.0)
+
+    def test_when_all_float_1_then_one(self):
+        self.assertEqual(calculate_age_average({'a':{'Age':1.0}, 'b':{'Age':1.0}, 'b':{'Age':1.0}}), 1.0)
+
+    def test_when_all_string_1_then_one(self):
+        self.assertEqual(calculate_age_average({'a':{'Age':'1'}, 'b':{'Age':'1'}, 'b':{'Age':'1'}}), 1.0)
+
+    def test_when_all_string_1pt0_then_one(self):
+        self.assertEqual(calculate_age_average({'a':{'Age':'1.0'}, 'b':{'Age':'1.0'}, 'b':{'Age':'1.0'}}), 1.0)
+    
+    def test_when_average_is_2_then_2(self):
+        self.assertEqual(calculate_age_average({'a':{'Age':1}, 'b':{'Age':2}, 'b':{'Age':3}}), 2.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Tag03/Übungen/the_gang.py
@@ -0,0 +1,63 @@
+import json
+import os
+
+
+def main():
+    json_file_name = "Tag03/Übungen/the_gang.json"
+    allGangMembers = loadFileContentFromJson(json_file_name)
+
+    averageAge = calculateAverageAge(allGangMembers)
+    print(f"The average age of all gang members is {averageAge} years.")
+
+    hairColors = getHairColors(allGangMembers)
+    print(f"The hair colors of all gang members are: {hairColors}")
+
+    saveResultsToJson(averageAge, hairColors)
+
+
+# Get the file json file content and load it into a dictionary
+def loadFileContentFromJson(json_file_name):
+    result = dict()
+    # Check if the given file exists
+    if os.path.exists(json_file_name):
+        with open(json_file_name, "r") as f:
+            result = json.load(f)
+    return result
+
+
+# Calculate the average age of all gang members
+def calculateAverageAge(allGangMembers):
+    sumOfAllAges = 0
+    # Iterate over all gang members.
+    for key, value in allGangMembers.items():
+        sumOfAllAges += value["Age"]
+    return sumOfAllAges / len(allGangMembers)
+
+
+# Get all hair colors of all gang members
+def getHairColors(allGangMembers):
+    hairColors = dict()
+    for key, value in allGangMembers.items():
+        if value["Hair"] in hairColors:
+            hairColors[value["Hair"]] += 1
+        else:
+            hairColors[value["Hair"]] = 1
+    return hairColors
+
+
+# Save the results to a json file
+def saveResultsToJson(averageAge, hairColors):
+    path = "Tag03/Übungen/the_gang_result.json"
+    result = dict()
+    result["AverageAge"] = averageAge
+    result["HairColors"] = hairColors
+
+    if os.path.exists(path):
+        os.remove(path)
+
+    with open(path, "w") as file:
+        json.dump(result, file)
+
+
+if __name__ == "__main__":
+    main()
